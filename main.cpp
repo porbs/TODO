28c0e125e221782fc302788da3b72d720ddd3691
Yevhenii Kubiuk <yevhenii.kubiuk@gmail.com>
2018-04-04 14:00:29 +0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
-#include "include/Board.h"
+//#include "Board.h"
+//#include "Filesystem.h"
+
+#include "TodoControllers.h"
 
 int main() {
-    Board board("Board 1");
+    todo::createBoard("Board 1");
+    todo::createBoard("Board 2");
+    todo::createBoard("Board 3");
 
-    board.addTask(Task("Task 1", "Description 1"));
-    board.addTask(Task("Task 2", "Description 2"));
+    todo::createBoard("Invalid board");
+    todo::removeBoard("Invalid board");
 
-    std::cout << board.toJson() << std::endl;
+    for (auto &board: todo::getBoardsList()){
+        board.addTask(Task("Task 1", "Task description..."));
+        todo::saveBoard(board);
+    }
 
     return 0;
 }
